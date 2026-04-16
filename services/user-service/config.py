98e9c8b5866a26d896fb8af557e0050662b6ec92
Hamad AlGhanim <haal5372@colorado.edu>
2026-04-16 14:28:36 -0600

    switch all services to use one database

--- services/user-service/config.py
+++ services/user-service/config.py
@@ -4,12 +4,14 @@ from db import Base
 import os
 
 
-DB_NAME = os.getenv("DB_NAME", "users_db.sqlite")
-DATABASE_URL = f"sqlite:////data/{DB_NAME}"
+DATABASE_URL = os.getenv("DATABASE_URL", f"sqlite:////data/{os.getenv('DB_NAME', 'users_db.sqlite')}")
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
